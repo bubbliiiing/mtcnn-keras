39afa9a91e3d127dc6b58959c40233463e6fbee6
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-01-30 12:28:58 +0800

    Add files via upload

--- detect.py
+++ detect.py
@@ -1,31 +1,33 @@
 import cv2
 import numpy as np
+
 from mtcnn import mtcnn
 
-img = cv2.imread('img/timg.jpg')
+if __name__ == "__main__":
+    model = mtcnn()
+    #-------------------------------------#
+    #   设置检测门限
+    #-------------------------------------#
+    threshold = [0.5,0.6,0.7]
+    #-------------------------------------#
+    #   读取图片
+    #-------------------------------------#
+    img = cv2.imread('img/timg.jpg')
+    #-------------------------------------#
+    #   将图片传入并检测
+    #-------------------------------------#
+    rectangles = model.detectFace(img, threshold)
 
-model = mtcnn()
-threshold = [0.5,0.6,0.7]
-rectangles = model.detectFace(img, threshold)
-draw = img.copy()
+    draw = img.copy()
+    for rectangle in rectangles:
+        W = int(rectangle[2]) - int(rectangle[0])
+        H = int(rectangle[3]) - int(rectangle[1])
 
-for rectangle in rectangles:
-    if rectangle is not None:
-        W = -int(rectangle[0]) + int(rectangle[2])
-        H = -int(rectangle[1]) + int(rectangle[3])
-        paddingH = 0.01 * W
-        paddingW = 0.02 * H
-        crop_img = img[int(rectangle[1]+paddingH):int(rectangle[3]-paddingH), int(rectangle[0]-paddingW):int(rectangle[2]+paddingW)]
-        if crop_img is None:
-            continue
-        if crop_img.shape[0] < 0 or crop_img.shape[1] < 0:
-            continue
-        cv2.rectangle(draw, (int(rectangle[0]), int(rectangle[1])), (int(rectangle[2]), int(rectangle[3])), (255, 0, 0), 1)
+        cv2.rectangle(draw, (int(rectangle[0]), int(rectangle[1])), (int(rectangle[2]), int(rectangle[3])), (0, 0, 255), 2)
 
         for i in range(5, 15, 2):
-            cv2.circle(draw, (int(rectangle[i + 0]), int(rectangle[i + 1])), 2, (0, 255, 0))
+            cv2.circle(draw, (int(rectangle[i + 0]), int(rectangle[i + 1])), 1, (255, 0, 0), 4)
 
-cv2.imwrite("img/out.jpg",draw)
-
-cv2.imshow("test", draw)
-c = cv2.waitKey(0)
+    cv2.imwrite("img/out.jpg",draw)
+    cv2.imshow("test", draw)
+    c = cv2.waitKey(0)
--- mtcnn.py
+++ mtcnn.py
@@ -1,18 +1,22 @@
-from keras.layers import Conv2D, Input,MaxPool2D, Reshape,Activation,Flatten, Dense, Permute
+import cv2
+import numpy as np
+import tensorflow as tf
+from keras.layers import (Activation, Conv2D, Dense, Flatten, Input, MaxPool2D,
+                          Permute, Reshape)
 from keras.layers.advanced_activations import PReLU
 from keras.models import Model, Sequential
-import tensorflow as tf
-import numpy as np
+
 import utils
-import cv2
+
+
 #-----------------------------#
 #   粗略获取人脸框
 #   输出bbox位置和是否有人脸
 #-----------------------------#
 def create_Pnet(weight_path):
-    input = Input(shape=[None, None, 3])
+    inputs = Input(shape=[None, None, 3])
 
-    x = Conv2D(10, (3, 3), strides=1, padding='valid', name='conv1')(input)
+    x = Conv2D(10, (3, 3), strides=1, padding='valid', name='conv1')(inputs)
     x = PReLU(shared_axes=[1,2],name='PReLU1')(x)
     x = MaxPool2D(pool_size=2)(x)
 
@@ -26,7 +30,8 @@ def create_Pnet(weight_path):
     # 无激活函数，线性。
     bbox_regress = Conv2D(4, (1, 1), name='conv4-2')(x)
 
-    model = Model([input], [classifier, bbox_regress])
+    model = Model([inputs], [classifier, bbox_regress])
+    model.summary()
     model.load_weights(weight_path, by_name=True)
     return model
 
@@ -35,13 +40,13 @@ def create_Pnet(weight_path):
 #   精修框
 #-----------------------------#
 def create_Rnet(weight_path):
-    input = Input(shape=[24, 24, 3])
-    # 24,24,3 -> 11,11,28
-    x = Conv2D(28, (3, 3), strides=1, padding='valid', name='conv1')(input)
+    inputs = Input(shape=[24, 24, 3])
+    # 24,24,3 -> 22,22,28 -> 11,11,28
+    x = Conv2D(28, (3, 3), strides=1, padding='valid', name='conv1')(inputs)
     x = PReLU(shared_axes=[1, 2], name='prelu1')(x)
     x = MaxPool2D(pool_size=3,strides=2, padding='same')(x)
 
-    # 11,11,28 -> 4,4,48
+    # 11,11,28 -> 9,9,48 -> 4,4,48
     x = Conv2D(48, (3, 3), strides=1, padding='valid', name='conv2')(x)
     x = PReLU(shared_axes=[1, 2], name='prelu2')(x)
     x = MaxPool2D(pool_size=3, strides=2)(x)
@@ -49,16 +54,21 @@ def create_Rnet(weight_path):
     # 4,4,48 -> 3,3,64
     x = Conv2D(64, (2, 2), strides=1, padding='valid', name='conv3')(x)
     x = PReLU(shared_axes=[1, 2], name='prelu3')(x)
+
     # 3,3,64 -> 64,3,3
     x = Permute((3, 2, 1))(x)
     x = Flatten()(x)
+
     # 576 -> 128
     x = Dense(128, name='conv4')(x)
     x = PReLU( name='prelu4')(x)
-    # 128 -> 2 128 -> 4
+
+    # 128 -> 2
     classifier = Dense(2, activation='softmax', name='conv5-1')(x)
+    # 128 -> 4
     bbox_regress = Dense(4, name='conv5-2')(x)
-    model = Model([input], [classifier, bbox_regress])
+
+    model = Model([inputs], [classifier, bbox_regress])
     model.load_weights(weight_path, by_name=True)
     return model
 
@@ -67,39 +77,43 @@ def create_Rnet(weight_path):
 #   精修框并获得五个点
 #-----------------------------#
 def create_Onet(weight_path):
-    input = Input(shape = [48,48,3])
-    # 48,48,3 -> 23,23,32
-    x = Conv2D(32, (3, 3), strides=1, padding='valid', name='conv1')(input)
+    inputs = Input(shape = [48,48,3])
+    # 48,48,3 -> 46,46,32 -> 23,23,32
+    x = Conv2D(32, (3, 3), strides=1, padding='valid', name='conv1')(inputs)
     x = PReLU(shared_axes=[1,2],name='prelu1')(x)
     x = MaxPool2D(pool_size=3, strides=2, padding='same')(x)
-    # 23,23,32 -> 10,10,64
+
+    # 23,23,32 -> 21,21,64 -> 10,10,64
     x = Conv2D(64, (3, 3), strides=1, padding='valid', name='conv2')(x)
     x = PReLU(shared_axes=[1,2],name='prelu2')(x)
     x = MaxPool2D(pool_size=3, strides=2)(x)
+
     # 8,8,64 -> 4,4,64
     x = Conv2D(64, (3, 3), strides=1, padding='valid', name='conv3')(x)
     x = PReLU(shared_axes=[1,2],name='prelu3')(x)
     x = MaxPool2D(pool_size=2)(x)
+
     # 4,4,64 -> 3,3,128
     x = Conv2D(128, (2, 2), strides=1, padding='valid', name='conv4')(x)
     x = PReLU(shared_axes=[1,2],name='prelu4')(x)
+
     # 3,3,128 -> 128,12,12
     x = Permute((3,2,1))(x)
+    x = Flatten()(x)
 
     # 1152 -> 256
-    x = Flatten()(x)
     x = Dense(256, name='conv5') (x)
     x = PReLU(name='prelu5')(x)
 
-    # 鉴别
-    # 256 -> 2 256 -> 4 256 -> 10 
+    # 256 -> 2 
     classifier = Dense(2, activation='softmax',name='conv6-1')(x)
+    # 256 -> 4 
     bbox_regress = Dense(4,name='conv6-2')(x)
+    # 256 -> 10 
     landmark_regress = Dense(10,name='conv6-3')(x)
 
-    model = Model([input], [classifier, bbox_regress, landmark_regress])
+    model = Model([inputs], [classifier, bbox_regress, landmark_regress])
     model.load_weights(weight_path, by_name=True)
-
     return model
 
 class mtcnn():
@@ -129,28 +143,43 @@ class mtcnn():
             hs = int(origin_h * scale)
             ws = int(origin_w * scale)
             scale_img = cv2.resize(copy_img, (ws, hs))
-            inputs = scale_img.reshape(1, *scale_img.shape)
+            inputs = np.expand_dims(scale_img, 0)
             ouput = self.Pnet.predict(inputs)
+            #---------------------------------------------#
+            #   每次选取图像金字塔中的一张图片进行预测
+            #   预测结果也是一张图片的，
+            #   所以我们可以将对应的batch_size维度给消除掉
+            #---------------------------------------------#
+            ouput = [ouput[0][0], ouput[1][0]]
             out.append(ouput)
 
-        image_num = len(scales)
         rectangles = []
-        for i in range(image_num):
-            # 有人脸的概率
-            cls_prob = out[i][0][0][:,:,1]
-            # 其对应的框的位置
-            roi = out[i][1][0]
-
-            # 取出每个缩放后图片的长宽
+        #-------------------------------------------------#
+        #   在这个地方我们对图像金字塔的预测结果进行循环
+        #   取出每张图片的种类预测和回归预测结果
+        #-------------------------------------------------#
+        for i in range(len(scales)):
+            #------------------------------------------------------------------#
+            #   为了方便理解，这里和视频上看到的不太一样
+            #   因为我们在上面对图像金字塔循环的时候就把batch_size维度给去掉了
+            #------------------------------------------------------------------#
+            cls_prob = out[i][0][:, :, 1]
+            roi = out[i][1]
+            #-------------------------------------#
+            #   取出每个缩放后图片的高宽
+            #-------------------------------------#
             out_h, out_w = cls_prob.shape
             out_side = max(out_h, out_w)
-            print(cls_prob.shape)
-            # 解码过程
+            #-------------------------------------#
+            #   解码的过程
+            #-------------------------------------#
             rectangle = utils.detect_face_12net(cls_prob, roi, out_side, 1 / scales[i], origin_w, origin_h, threshold[0])
             rectangles.extend(rectangle)
 
-        # 进行非极大抑制
-        rectangles = utils.NMS(rectangles, 0.7)
+        #-------------------------------------#
+        #   进行非极大抑制
+        #-------------------------------------#
+        rectangles = np.array(utils.NMS(rectangles, 0.7))
 
         if len(rectangles) == 0:
             return rectangles
@@ -161,17 +190,20 @@ class mtcnn():
         #-----------------------------#
         predict_24_batch = []
         for rectangle in rectangles:
+            #------------------------------------------#
+            #   利用获取到的粗略坐标，在原图上进行截取
+            #------------------------------------------#
             crop_img = copy_img[int(rectangle[1]):int(rectangle[3]), int(rectangle[0]):int(rectangle[2])]
+            #-----------------------------------------------#
+            #   将截取到的图片进行resize，调整成24x24的大小
+            #-----------------------------------------------#
             scale_img = cv2.resize(crop_img, (24, 24))
             predict_24_batch.append(scale_img)
 
-        predict_24_batch = np.array(predict_24_batch)
-        out = self.Rnet.predict(predict_24_batch)
-
-        cls_prob = out[0]
-        cls_prob = np.array(cls_prob)
-        roi_prob = out[1]
-        roi_prob = np.array(roi_prob)
+        cls_prob, roi_prob = self.Rnet.predict(np.array(predict_24_batch))
+        #-------------------------------------#
+        #   解码的过程
+        #-------------------------------------#
         rectangles = utils.filter_face_24net(cls_prob, roi_prob, rectangles, origin_w, origin_h, threshold[1])
 
         if len(rectangles) == 0:
@@ -183,16 +215,21 @@ class mtcnn():
         #-----------------------------#
         predict_batch = []
         for rectangle in rectangles:
+            #------------------------------------------#
+            #   利用获取到的粗略坐标，在原图上进行截取
+            #------------------------------------------#
             crop_img = copy_img[int(rectangle[1]):int(rectangle[3]), int(rectangle[0]):int(rectangle[2])]
+            #-----------------------------------------------#
+            #   将截取到的图片进行resize，调整成48x48的大小
+            #-----------------------------------------------#
             scale_img = cv2.resize(crop_img, (48, 48))
             predict_batch.append(scale_img)
 
-        predict_batch = np.array(predict_batch)
-        output = self.Onet.predict(predict_batch)
-        cls_prob = output[0]
-        roi_prob = output[1]
-        pts_prob = output[2]
-
+        cls_prob, roi_prob, pts_prob = self.Onet.predict(np.array(predict_batch))
+        
+        #-------------------------------------#
+        #   解码的过程
+        #-------------------------------------#
         rectangles = utils.filter_face_48net(cls_prob, roi_prob, pts_prob, rectangles, origin_w, origin_h, threshold[2])
 
         return rectangles
